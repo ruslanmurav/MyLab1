0d17430d49bc10937a393c8842c474f431d28763
Ruslan Muraveiko <ruslanmuraveiko@gmail.com>
2022-12-16 10:59:25 +0300

    Добавьте файлы проекта.

---
+++ MyLab1/MyLab1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+void main()
+{
+    setlocale(LC_ALL, "RU");
+    double x = 0.4 * pow(10, 4), y = -0.875, z = -0.475 * pow(10, -3), s;
+    s = pow(abs(cos(x) - cos(y)), (1 + 2 * sin(y) * sin(y))) * (1 + z + pow(z, 2) / 2 + pow(z, 3) / 3 + pow(z, 4) / 4);
+    cout << s;
+}
